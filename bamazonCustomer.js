7b3778ed910ab661dcde8f6a7e0a8f60757b68f1
mrgmacandog <24902881+mrgmacandog@users.noreply.github.com>
2019-05-13 00:23:04 -0700

    Add ability to connect to DB and log products table

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -4,5 +4,30 @@ require("dotenv").config();
 // Include the keys package
 const keys = require("./keys.js");
 
-// Test to access MySQL root password
-console.log(keys.rootPassword);
+// Include the mysql package
+const mysql = require("mysql");
+
+// Create the way to refer the bamazon database
+const connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: keys.rootPassword,
+    database: "bamazon"
+});
+
+// Connect to the bamazon database
+connection.connect(function (err) {
+    // If there is an error, throw it
+    if (err) throw err;
+
+    connection.query(
+        "SELECT * FROM products",
+        function (err, res) {
+            if (err) throw err;
+            console.log(res);
+        }
+    );
+
+    connection.end();
+});
