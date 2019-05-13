686050d28823bcb41510c74a2fe8980e7ebaceab
mrgmacandog <24902881+mrgmacandog@users.noreply.github.com>
2019-05-12 23:46:03 -0700

    Create seeds.sql to populate products

---
+++ seeds.sql
@@ -0,0 +1,15 @@
+USE bamazon;
+
+INSERT INTO products
+    (product_name, department_name, price, stock_quantity)
+VALUES
+    ("Sunglasses", "Accessories", 89.99, 100),
+    ("Watch", "Accessories", 79.99, 75),
+    ("Shorts", "Clothing", 19.99, 350),
+    ("Hoodie", "Clothing", 29.99, 240),
+    ("Sweater", "Clothing", 26.99, 215),
+    ("Guitar", "Musical Instruments", 199.99, 50),
+    ("Piano", "Musical Instruments", 259.99, 35),
+    ("Slow Cooker", "Appliances", 49.99, 220),
+    ("Deep Fryer", "Appliances", 31.99, 185),
+    ("Toaster", "Appliances", 22.99, 280);
